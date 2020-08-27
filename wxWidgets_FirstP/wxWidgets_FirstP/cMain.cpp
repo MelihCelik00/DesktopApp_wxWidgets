7e2bc233081b5b8910fb11e8cd6ba703525ae66f
Melih Safa Çelik <melihsafa.c@gmail.com>
2020-08-27 15:16:05 +0300

    First update

---
+++ wxWidgets_FirstP/wxWidgets_FirstP/cMain.cpp
@@ -0,0 +1,11 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "AXA Sigorta Rover Control", wxPoint(30,30), wxSize(800,600))
+{
+
+}
+
+cMain::~cMain()
+{
+
+}
---
+++ wxWidgets_FirstP/wxWidgets_FirstP/cMain.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "wx/wx.h"
+
+class cMain : public wxFrame
+{
+
+public:
+	cMain();
+	~cMain();
+
+};
+
